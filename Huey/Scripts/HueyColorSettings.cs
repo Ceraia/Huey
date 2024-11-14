61136f04f7a68b01cb2fdeadcddebf0e7a0d8698
Axodouble <git@axodouble.com>
2024-11-14 23:23:22 +0100

    Release of Huey v2 Unity Edition

---
+++ Huey/Scripts/HueyColorSettings.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ColorSettings", menuName = "Huey/Color Settings")]
+public class HueyColorSettings : ScriptableObject
+{
+    [System.Serializable]
+    public struct ColorSettings
+    {
+        public string name;
+        [Range(0, 360)] public float hueShift;
+        [Range(0, 1)] public float saturationScale;
+        [Range(-100, 100)] public float lightnessPercent;
+    }
+
+    public ColorSettings[] colors;
+}
